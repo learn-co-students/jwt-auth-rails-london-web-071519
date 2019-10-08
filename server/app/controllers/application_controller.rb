bbc9c5cf7a3bb20ead968be68fb5fe5356c8b51f
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-10-08 14:33:52 +0100

    Done.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ class ApplicationController < ActionController::API
       token = auth_header.split(' ')[1] #[Bearer, <token>]
       begin
         JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+
+        
         # JWT.decode => [{ "user_id"=>"2" }, { "alg"=>"HS256" }]
       rescue JWT::DecodeError
         nil
